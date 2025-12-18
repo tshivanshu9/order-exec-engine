7d81c3ec9fca1682855b18cf6d229db6bb5ba52b
tshivanshu9 <tshivanshu9@gmail.com>
2025-12-18 20:57:22 +0530

    chore: Pg change

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export const pgPool = new Pool({
   host: process.env.PG_HOST,
-  port: 5432,
+  port: process.env.PG_PORT ? Number(process.env.PG_PORT) : 5432,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
